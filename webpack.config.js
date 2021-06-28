a738ea6b795ab1eae1c3e5b18462f131ebc72734
daniel walters <daniel.walters2110@gmail.com>
2021-06-28 17:07:29 +0300

    clean build

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Atlas Coffee',
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    //publicPath: '/',
   },
   module: {
       rules: [
