d5d5062af92a4d3434b692e1e55c73aee5f7f824
daniel walters <daniel.walters2110@gmail.com>
2021-06-28 17:04:43 +0300

    :wq :q
    wwwe
    
    q
    quit
    quit()

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-
-  plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Atlas Coffee',
-    }),
-  ],
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, '/'),
-    clean: true,
-    //publicPath: '/',
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
       rules: [
